ea0dc5aa4e61560dff04de05eac2f9e1111a1c15
NakilaBrand <nicolas.br@hotmail.fr>
2020-02-19 16:59:43 +0100

    Update EventController.php
    etat stable mais en en dur dans le code

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class EventController extends AbstractController
 {
     /**
      * @Route("/event", name="event")
      */
-    public function index()
+    public function listEvents(EntityManagerInterface $em)
     {
+        $list = $em->getRepository(Event::class)->findAllByPage(1, 10);
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'list' => $list
         ]);
     }
 }
