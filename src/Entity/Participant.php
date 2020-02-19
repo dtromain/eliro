5bb5f17da454692caaeeaaf87867227c7818480a
NakilaBrand <nicolas.br@hotmail.fr>
2020-02-19 11:07:29 +0100

    ajout api

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,8 +6,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ *  @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\ParticipantRepository")
  */
 class Participant implements UserInterface
