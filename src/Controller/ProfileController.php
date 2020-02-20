43951dca2f3c9eb110f8ee00a5b705f936f562f6
Romain Douteau <personnel@romaindouteau.fr>
2020-02-20 12:12:22 +0100

    Fix password changes errors

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ProfileFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,17 +27,20 @@ class ProfileController extends AbstractController
         $form = $this->createForm(ProfileFormType::class, $participant);
 
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        dump(__LINE__);
 
+        $old_password = $form->get('old_password')->getData();
+
+        if($old_password && !$encoder->isPasswordValid($participant, $old_password)) {
+            $form->get('old_password')->addError(new FormError('Invalid password'));
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
             $plain_password = $form->get('plain_password')->getData();
 
-            if($plain_password) {
-                if($encoder->isPasswordValid($participant, $form->get('old_password')->getData())) {
-                    $encoded_password = $encoder->encodePassword($participant, $plain_password);
-                    $participant->setPassword($encoded_password);
-                } else {
-
-                }
+            if($plain_password && $old_password) {
+                $encoded_password = $encoder->encodePassword($participant, $plain_password);
+                $participant->setPassword($encoded_password);
             }
 
             $entityManager = $this->getDoctrine()->getManager();
