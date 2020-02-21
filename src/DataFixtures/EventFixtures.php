9c918e03e77ed015facdbd67a0a71e7413a6d89c
NakilaBrand <nicolas.br@hotmail.fr>
2020-02-21 10:30:37 +0100

    Update EventFixtures.php
    ajout des parametres état et campus de facon aleatoire dans event

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -30,6 +30,38 @@ class EventFixtures extends Fixture
 
             $event->setInformation('');
 
+            switch (rand(0,3)) {
+                case 0:
+                    $event->setState($this->getReference(CityFixtures::STATE_CREATING_REFERENCE));
+                    break;
+                case 1:
+                    $event->setState($this->getReference(CityFixtures::STATE_OPEN_REFERENCE));
+                    break;
+                case 2:
+                    $event->setState($this->getReference(CityFixtures::STATE_CLOSE_REFERENCE));
+                    break;
+                case 3:
+                    $event->setState($this->getReference(CityFixtures::STATE_ONGOING_REFERENCE));
+                    break;
+
+            }
+
+            switch (rand(0,3)) {
+                case 0:
+                    $event->setCampus($this->getReference(CityFixtures::CAMPUS_NANTES_REFERENCE));
+                    break;
+                case 1:
+                    $event->setCampus($this->getReference(CityFixtures::CITY_QUIMPER_REFERENCE));
+                    break;
+                case 2:
+                    $event->setCampus($this->getReference(CityFixtures::CAMPUS_RENNES_REFERENCE));
+                    break;
+                case 3:
+                    $event->setCampus($this->getReference(CityFixtures::CAMPUS_NIORT_REFERENCE));
+                    break;
+
+            }
+
             $manager->persist($event);
         }
 
